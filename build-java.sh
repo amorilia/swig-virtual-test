3624f4745a32d65c2e58397f564afe68e23490ac
Amorilia <amorilia@users.sourceforge.net>
2012-09-17 19:38:30 +0100

    Java include folder for mac (contributed by elf).

--- build-java.sh
+++ build-java.sh
@@ -1,5 +1,5 @@
 swig -Wall -c++ -java testmod.i
-g++ -fPIC -c testmod_wrap.cxx -o testmod_wrap.o -I/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.6.x86_64/include/ -I/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.6.x86_64/include/linux/ -I.
+g++ -fPIC -c testmod_wrap.cxx -o testmod_wrap.o -I/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.6.x86_64/include/ -I/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.6.x86_64/include/linux/ -I/System/Library/Frameworks/JavaVM.framework/Headers -I.
 g++ -shared testmod_wrap.o -o libtestmod.so
 javac *.java
 
