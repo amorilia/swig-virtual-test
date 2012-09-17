59e804187da5034df89c6c17650d3007431ab9e3
Amorilia <amorilia@users.sourceforge.net>
2012-09-17 18:36:42 +0100

    Include command for running java test in build script.

--- build-java.sh
+++ build-java.sh
@@ -2,3 +2,8 @@ swig -Wall -c++ -java testmod.i
 g++ -fPIC -c testmod_wrap.cxx -o testmod_wrap.o -I/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.6.x86_64/include/ -I/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.6.x86_64/include/linux/ -I.
 g++ -shared testmod_wrap.o -o libtestmod.so
 javac *.java
+
+# run it as well
+# must set java.library.path to the path where libtestmod.so resides
+java -Djava.library.path=. Test
+
