b7c7f4c463358bc6a353215631a2d59b954cb016
Amorilia <amorilia@users.sourceforge.net>
2012-09-17 20:10:33 +0100

    Initial csharp test, with compile and run in build script.

--- build-csharp.sh
+++ build-csharp.sh
@@ -1,3 +1,9 @@
 swig -Wall -c++ -csharp testmod.i
 g++ -fPIC -c testmod_wrap.cxx -o testmod_wrap.o -I.
 g++ -shared testmod_wrap.o -o libtestmod.so
+mcs test.cs testmod.cs TestObj.cs testmodPINVOKE.cs
+mono test.exe
+
+# TODO set up a library
+#mcs /target:library /out:TestMod.dll testmod.cs TestObj.cs testmodPINVOKE.cs
+
